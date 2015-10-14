e91f99e68f982a31f0894cb141eb5869d7f5df34
payma <fateme.abiri@yahoo.com>
2015-10-14 09:09:02 +0330

    touch long on a patient in list view...(Edit,view,delete) only delete action was writened action bar menu...

--- app/src/main/java/fuzzium/nursys/activities/SelectPatientActivity.java
+++ app/src/main/java/fuzzium/nursys/activities/SelectPatientActivity.java
@@ -1,14 +1,19 @@
 package fuzzium.nursys.activities;
 
+import android.annotation.TargetApi;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,10 +25,9 @@ public class SelectPatientActivity extends ActionBarActivity {
     private ListView listView;
     private List<Patient> patients;
     private EditText searchPatient;
-
-
+    private Patient selectedPatient;
+    @TargetApi(11)
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_patient);
@@ -36,6 +40,67 @@ public class SelectPatientActivity extends ActionBarActivity {
         patients = Patient.listAll(Patient.class);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, patients);
         listView.setAdapter(adapter);
+
+
+        final ActionMode.Callback modeCallBack = new ActionMode.Callback() {
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            public void onDestroyActionMode(ActionMode mode) {
+                mode = null;
+                getSupportActionBar().show();
+
+            }
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+
+
+                getSupportActionBar().hide();
+                mode.getMenuInflater().inflate(R.menu.menu_select_patient, menu);
+                return true;
+
+            }
+
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.view: {
+                        Toast.makeText(getApplicationContext(), "Item ... ! "+ selectedPatient.getFname()+" "+selectedPatient.getLname(), Toast.LENGTH_LONG).show();
+                        mode.finish();
+                        break;
+                    }
+                    case R.id.delete: {
+                        Toast.makeText(getApplicationContext(), "Item  deleted! "+selectedPatient.getFname()+" "+selectedPatient.getLname(), Toast.LENGTH_LONG).show();
+                        selectedPatient.delete();
+                        mode.finish();
+
+                        break;
+                    }
+                    case R.id.edit: {
+                        Toast.makeText(getApplicationContext(), "Item  edited! "+selectedPatient.getFname()+" "+selectedPatient.getLname(), Toast.LENGTH_LONG).show();
+                        mode.finish();
+                        break;
+                    }
+                    default:
+                        return false;
+                }
+
+                return true;
+
+            }
+        };
+
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView parent, View view, int position, long id) {
+                startActionMode(modeCallBack);
+                view.setSelected(true);
+                view.getFocusables(position);
+                selectedPatient =(Patient) (listView.getItemAtPosition(position));
+                return true;
+            }
+        });
 
 
         listView.setTextFilterEnabled(true);
@@ -71,7 +136,7 @@ public class SelectPatientActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_select_patient, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -83,10 +148,14 @@ public class SelectPatientActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.Item1) {
+            Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.Item2) {
+            Toast.makeText(getApplicationContext(), "Item 2 Selected", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.Item3) {
+            Toast.makeText(getApplicationContext(), "Item 2 Selected", Toast.LENGTH_LONG).show();
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
+
